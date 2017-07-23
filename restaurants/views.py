87c8902ee5ba0fa75ff62c8043e47b2792d3a3b1
Vlad M <vladpochta5@gmail.com>
2017-07-23 21:35:45 +0300

    8 - Context in Django Templates

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,16 @@
+import random
+
 from django.shortcuts import render
 
 
 # function based view
 def home(request):
-    return render(request, "base.html", {})
+    num = random.randint(0, 100000000)
+    some_list = [num, random.randint(0, 100000000)]
+    context = {
+        'html_var': False,
+        'num': num,
+        'some_list': some_list
+    }
+    return render(request, "base.html", context)
 
