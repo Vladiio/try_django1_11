30a3694a693cdb29b47f58e618f3d836f15ec834
Vlad M <vladpochta5@gmail.com>
2017-07-24 17:52:59 +0300

    12 - Class Based Views

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home
+from restaurants import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^$', views.home),
+    url(r'^contact/(?P<id>\d+)/$', views.ContactView.as_view()),
+    url(r'^about/$', views.about),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.shortcuts import render
+from django.views import View
 
 
 # function based view
@@ -8,9 +9,28 @@ def home(request):
     num = random.randint(0, 100000000)
     some_list = [num, random.randint(0, 100000000)]
     context = {
-        'html_var': False,
+        'html_var': True,
         'num': num,
         'some_list': some_list
     }
-    return render(request, "base.html", context)
+    return render(request, "home.html", context)
 
+
+def about(request):
+    return render(request, "about.html", {})
+
+
+def contact(request):
+    return render(request, "contact.html", {})
+
+
+# class-based views
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "contact.html", {})
+
+    # def post(self, request, *args, **kwargs):
+    #     return render(request, "contact.html", {})
+
+    # def put(self, request, *args, **kwargs):
+    #     return render(request, "contact.html", {})
